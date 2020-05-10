76fd3adc17c9a03c13a2c17ac3f9cf786abf6734
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2020-05-10 07:51:29 +0530

    Correcting run script generation

--- prepare-package.sh
+++ prepare-package.sh
@@ -7,6 +7,6 @@ files=( *.jar )
 cd ..
 cp target/*.jar /tmp/app/
 cp deployment/etc/* /tmp/app/
-echo "#!/bin/sh\n\njava -jar ${files[0]}" > /tmp/app/run.sh
+echo -e "#!/bin/bash\n\njava -jar ${files[0]}" > /tmp/app/run.sh
 chmod a+x /tmp/app/run.sh
 zip -r -j spring-boot-deployment.zip /tmp/app/*.*
