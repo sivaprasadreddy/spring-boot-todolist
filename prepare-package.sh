651a9e8b083f0cde4eef8cd6808f4ca5308d404c
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2020-05-10 07:38:20 +0530

    Adding package generation

--- prepare-package.sh
+++ prepare-package.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-mvn clean install -DskipTests
+mvn clean package -DskipTests
 mkdir -p /tmp/app/
 cd ./target
 files=( *.jar )
-#echo "${files[0]}"
 cd ..
 cp target/*.jar /tmp/app/
 cp deployment/etc/* /tmp/app/
 echo "#!/bin/sh\n\njava -jar ${files[0]}" > /tmp/app/run.sh
-
+chmod a+x /tmp/app/run.sh
 zip -r -j spring-boot-deployment.zip /tmp/app/*.*
