4994ca9dbbb9c1d2aba8e4e2d2f8e326bddd09a8
sivaprasadreddy <sivaprasadreddy.k@gmail.com>
2021-11-04 06:18:53 +0530

    Fix formatting issue

--- src/test/java/com/sivalabs/todolist/ApplicationIT.java
+++ src/test/java/com/sivalabs/todolist/ApplicationIT.java
@@ -1,12 +1,10 @@
 package com.sivalabs.todolist;
 
-
 import com.sivalabs.todolist.common.AbstractIntegrationTest;
 import org.junit.jupiter.api.Test;
 
 class ApplicationIT extends AbstractIntegrationTest {
 
     @Test
-    void contextLoads() {
-    }
+    void contextLoads() {}
 }
