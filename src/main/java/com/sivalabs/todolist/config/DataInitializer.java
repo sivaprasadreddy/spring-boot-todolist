1560ebae8643f937cdf3f08bb28dde316136229c
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2022-06-24 09:21:44 +0530

    Fix h2 migration, upgrade UI libraries

---
+++ src/main/java/com/sivalabs/todolist/config/DataInitializer.java
@@ -0,0 +1,42 @@
+package com.sivalabs.todolist.config;
+
+import com.sivalabs.todolist.domain.Todo;
+import com.sivalabs.todolist.domain.TodoService;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class DataInitializer implements CommandLineRunner {
+    private static final List<String> SAMPLE_TODOS =
+            List.of(
+                    "Learn SpringBoot",
+                    "Learn VueJS",
+                    "Write blog post on GitHub Actions",
+                    "Learn Kubernetes",
+                    "Upgrade Blog",
+                    "Publish YouTube video on Quarkus");
+
+    private final ApplicationProperties properties;
+    private final TodoService todoService;
+
+    @Override
+    public void run(String... args) {
+        if (!properties.isInitSampleData()) {
+            log.info("Sample data initialization is disabled");
+            return;
+        }
+        SAMPLE_TODOS.forEach(
+                t -> {
+                    Todo todo = new Todo();
+                    todo.setContent(t);
+                    todo.setCreatedAt(LocalDateTime.now());
+                    todoService.saveTodo(todo);
+                });
+    }
+}
--- src/main/resources/db/migration/h2/V1__create_tables.sql
+++ src/main/resources/db/migration/h2/V1__create_tables.sql
@@ -1,7 +1,7 @@
 create sequence todo_id_seq start with 1 increment by 10;
 
 create table todos (
-    id bigint default todo_id_seq.nextval,
+    id bigint default nextval('todo_id_seq') not null,
     content varchar(1024) not null,
     created_at timestamp,
     updated_at timestamp,
