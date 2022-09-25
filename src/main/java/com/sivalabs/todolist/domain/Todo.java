48c5d25ee5c3a61dd24e41db5666bd4fac3a9ea6
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2022-09-25 11:44:38 +0530

    Upgrade to springBoot 3

--- src/main/java/com/sivalabs/todolist/domain/Todo.java
+++ src/main/java/com/sivalabs/todolist/domain/Todo.java
@@ -1,16 +1,16 @@
 package com.sivalabs.todolist.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
 import java.io.Serializable;
 import java.time.LocalDateTime;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.PrePersist;
-import javax.persistence.PreUpdate;
-import javax.persistence.SequenceGenerator;
-import javax.persistence.Table;
-import javax.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
