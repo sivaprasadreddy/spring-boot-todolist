8c65be9125dcafb4ee84910250034d7e774845d8
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2024-03-23 13:13:30 +0530

    Updated helm chart

--- src/main/java/com/sivalabs/todolist/TodoListApplication.java
+++ src/main/java/com/sivalabs/todolist/TodoListApplication.java
@@ -3,7 +3,6 @@ package com.sivalabs.todolist;
 import com.sivalabs.todolist.config.ApplicationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
@@ -11,8 +10,6 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 public class TodoListApplication {
 
     public static void main(String[] args) {
-        SpringApplication springApplication = new SpringApplication(TodoListApplication.class);
-        springApplication.addListeners(new ApplicationPidFileWriter());
-        springApplication.run(args);
+        SpringApplication.run(TodoListApplication.class, args);
     }
 }
