6e44b91210770eea4802910473620294053e6824
K. Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2024-06-13 16:51:26 +0530

    Upgrade versions

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,45 +1,47 @@
-new Vue({
-    el: '#app',
-    data: {
-        todos: [],
-        newTodo: {}
-    },
-    created: function () {
-        this.loadTodos();
-    },
-    methods: {
-        loadTodos() {
-            let self = this;
-            $.getJSON("api/todos", function (data) {
-                self.todos = data
-            });
-        },
-
-        saveTodo() {
-            let self = this;
-
-            $.ajax({
-                type: "POST",
-                url: 'api/todos',
-                data: JSON.stringify(this.newTodo),
-                contentType: "application/json",
-                success: function () {
-                    self.newTodo = {};
-                    self.loadTodos();
-                }
-            });
-        },
-
-        deleteTodo(id) {
-            let self = this;
-
-            $.ajax({
-                type: "DELETE",
-                url: 'api/todos/' + id,
-                success: function () {
-                    self.loadTodos();
-                }
-            });
-        }
+const app = Vue.createApp({
+  data() {
+    return {
+      todos: [],
+      newTodo: {}
     }
+  },
+  created: function () {
+    this.loadTodos();
+  },
+  methods: {
+    loadTodos() {
+      let self = this;
+      $.getJSON("api/todos", function (data) {
+        self.todos = data
+      });
+    },
+
+    saveTodo() {
+      let self = this;
+
+      $.ajax({
+        type: "POST",
+        url: 'api/todos',
+        data: JSON.stringify(this.newTodo),
+        contentType: "application/json",
+        success: function () {
+          self.newTodo = {};
+          self.loadTodos();
+        }
+      });
+    },
+
+    deleteTodo(id) {
+      let self = this;
+
+      $.ajax({
+        type: "DELETE",
+        url: 'api/todos/' + id,
+        success: function () {
+          self.loadTodos();
+        }
+      });
+    }
+  }
 });
+app.mount("#app");
